0ce3620ab84539e1ea758f1b59a0d84c928eada7
DariuszSm <dariuszs2@nycstudents.net>
2024-04-07 12:12:10 -0400

    add additional camera views, add functionality to move camera between viewpoints

--- scenes/Functional/view_cam.gd
+++ scenes/Functional/view_cam.gd
@@ -4,14 +4,38 @@ extends Camera3D
 
 @export var raycast_result = {}
 
+@export var camPointsParent : Node
+var camPoints = {}
+var camIndex = 1
+var camSwitchProg = 0
+var oldCameraTransitionPosition
+var oldCameraTransitionRotation
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if camPointsParent != null:
+		camPoints = camPointsParent.get_children()
+		position = camPoints[0].position
+		rotation = camPoints[0].rotation
+		oldCameraTransitionPosition = position
+		oldCameraTransitionRotation = rotation
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	ScreenPointToRay()
+	print(camPoints)
+	camSwitchProg+=delta*4
+	if camSwitchProg > PI/2:
+		camSwitchProg = 0
+		camIndex += 1
+		if camIndex >= camPoints.size():
+			camIndex = 0
+		oldCameraTransitionPosition = position
+		oldCameraTransitionRotation = rotation
+	
+	if camPoints.size() != 0:
+		position = oldCameraTransitionPosition.lerp(camPoints[camIndex].position, sin(camSwitchProg))
+		rotation = oldCameraTransitionRotation.lerp(camPoints[camIndex].rotation, sin(camSwitchProg))
 
 func ScreenPointToRay():
 	var camera = get_tree().root.get_camera_3d()
